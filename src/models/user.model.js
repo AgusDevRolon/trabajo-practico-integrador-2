8371c84a14a50df3d36af8f4dc49944e5f5cb10b
Agustín Rolón <agustinxd364@gmail.com>
2025-09-26 10:08:18 -0300

    añadi la eliminacion logica en User.

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -13,8 +13,26 @@ const UserSchema = new Schema({
     email: {type: String, unique: true},
     password: {type: String},
     role: {type: String, enum: ["user", "admin"], default: "user"},
-    profile: ProfileSchema //doc embebido
+    profile: ProfileSchema, //doc embebido
+    deletedAt: {type: Date, default: null}
 }, {timestamps: true});
 
+//middleware para excluir  los eliminados
+function excluideDeleted(next){
+    if (!this.getOptions().skipDeleted){
+        this.where({deletedAt: null});
+    }
+    next();
+}
+
+UserSchema.pre("find", excluideDeleted);
+UserSchema.pre("findOne", excluideDeleted);
+UserSchema.pre("findOneAndUpdate", excluideDeleted);
+
+//metodo de soft delete
+UserSchema.methods.softDelete = async function (){
+    this.deletedAt = new Date();
+    return this.save();
+};
 
 export default model("User", UserSchema);
